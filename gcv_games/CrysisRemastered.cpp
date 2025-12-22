ab785aa690f7b33da3922ed248bcf732014b96ea
Jake <zikun.dai@icloud.com>
2025-12-22 15:29:16 +0800

    删掉CrysisRemastered一些注释

--- gcv_games/CrysisRemastered.cpp
+++ gcv_games/CrysisRemastered.cpp
@@ -23,45 +23,7 @@ bool GameCrysis::can_interpret_depth_buffer() const {
 static float g_far_plane_distance = FAR_PLANE_DISTANCE;
 
 float GameCrysis::convert_to_physical_distance_depth_u64(uint64_t depthval) const {
-	// static float far_plane_distance = FAR_PLANE_DISTANCE;
-	// if (!init_in_game()) return false;
-
-	// // read far plane
-	// const UINT_PTR dll_base = static_cast<UINT_PTR>(reinterpret_cast<uintptr_t>(camera_dll));
-	// float far_plane_read = FAR_PLANE_DISTANCE;
-	// SIZE_T nbytesread = 0;
-	// const uint64_t far_plane_offset = 0x23FB4E0ull;
-	// std::string err;
-	// if (tryreadmemory(gamename_verbose() + std::string("_far_plane"), err, mygame_handle_exe,
-	// 	(LPCVOID)(dll_base + far_plane_offset), reinterpret_cast<LPVOID>(&far_plane_read), sizeof(far_plane_read), &nbytesread)) {
-	// 	far_plane_distance = far_plane_read;
-	// } else if (!err.empty()) {
-	// 	reshade::log_message(reshade::log_level::warning, err.c_str());
-	// }
 	const float far_plane_distance = g_far_plane_distance;
-	// static bool far_plane_initialized = false;
-	// if (!far_plane_initialized) {
-	// 	far_plane_initialized = true;
-	// 	if (mygame_handle_exe == 0) {
-	// 		const_cast<GameCrysis*>(this)->init_get_game_exe();
-	// 	}
-	// 	HMODULE exe_module = (camera_dll != 0) ? camera_dll : GetModuleHandle(nullptr);
-	// 	if (exe_module != nullptr) {
-	// 		const void* far_plane_addr = reinterpret_cast<const void*>(reinterpret_cast<uintptr_t>(exe_module) + 0x23FB310ull);
-	// 		SIZE_T bytes_read = 0;
-	// 		float far_plane_read = FAR_PLANE_DISTANCE;
-	// 		std::string err;
-	// 		if (tryreadmemory(gamename_verbose() + std::string("_far_plane"), err, mygame_handle_exe,
-	// 			far_plane_addr, reinterpret_cast<LPVOID>(&far_plane_read), sizeof(far_plane_read), &bytes_read)) {
-	// 			far_plane_distance = far_plane_read;
-	// 			char logbuf[96];
-	// 			std::snprintf(logbuf, sizeof(logbuf), "[Crysis] far plane distance: %.6f", static_cast<double>(far_plane_distance));
-	// 			reshade::log_message(reshade::log_level::info, logbuf);
-	// 		} else if (!err.empty()) {
-	// 			reshade::log_message(reshade::log_level::warning, err.c_str());
-	// 		}
-	// 	}
-	// }
 	
 	// convert to physical distance
 	uint32_t depth_as_u32 = static_cast<uint32_t>(depthval);
@@ -81,57 +43,6 @@ bool GameCrysis::get_camera_matrix(CamMatrixData& rcam, std::string& errstr) {
 
 	const UINT_PTR dll_base = (UINT_PTR)camera_dll;
 	SIZE_T nbytesread = 0;
-
-	// // read pos
-	// const UINT_PTR dll_base = static_cast<UINT_PTR>(reinterpret_cast<uintptr_t>(camera_dll));
-	// float cambuf[12] = {};
-	// SIZE_T nbytesread = 0;
-	// const uint64_t cam_translation_offset = 0x23FB4E0ull;
-	// float cam_translation[3];
-	// if (!tryreadmemory(gamename_verbose() + std::string("_cam_translation"), errstr, mygame_handle_exe,
-	// 	(LPCVOID)(dll_base + cam_translation_offset), reinterpret_cast<LPVOID>(cam_translation),
-	// 	sizeof(cam_translation), &nbytesread)) {
-	// 	return false;
-	// }
-	// for (int row = 0; row < 3; ++row) {
-	// 	cambuf[row * 4 + 3] = cam_translation[row];
-	// }
-
-	// // convert to opengl style (right-handed)
-	// // cambuf[3] = cam_translation[0]; cambuf[7] = -cam_translation[2]; cambuf[11] = cam_translation[1];
-	
-	// // read angles
-	// float angbuf[3];
-	// const uint64_t ang_offset = 0x7EEA9C8ull;
-	// if (!tryreadmemory(gamename_verbose() + std::string("_angles"), errstr, mygame_handle_exe,
-	// 	(LPCVOID)(dll_base + ang_offset), reinterpret_cast<LPVOID>(angbuf),
-	// 	sizeof(angbuf), &nbytesread)) {
-	// 	return false;
-	// }
-	// const float yaw = angbuf[0];
-	// const float pitch = angbuf[1];
-	// const float roll = angbuf[2];
-	// const float cy = std::cos(yaw), sy = std::sin(yaw);
-	// const float cp = std::cos(pitch), sp = std::sin(pitch);
-	// const float cr = std::cos(roll), sr = std::sin(roll);
-
-	// const float r00 = cy * cr - sy * sp * sr;
-	// const float r01 = -sy * cp;
-	// const float r02 = cy * sr + sy * sp * cr;
-	// const float r10 = sy * cr + cy * sp * sr;
-	// const float r11 = cy * cp;
-	// const float r12 = sy * sr - cy * sp * cr;
-	// const float r20 = -cp * sr;
-	// const float r21 = sp;
-	// const float r22 = cp * cr;
-
-	// // convert to opengl style (right-handed)
-	// cambuf[0] = r00; cambuf[1] = r02; cambuf[2] = -r01;
-	// cambuf[4] = r10; cambuf[5] = r12; cambuf[6] = -r11;
-	// cambuf[8] = r20; cambuf[9] = r22; cambuf[10] = -r21;
-	// // cambuf[0] = r00; cambuf[1] = -r02; cambuf[2] = r01;
-	// // cambuf[4] = -r20; cambuf[5] = r22; cambuf[6] = -r21;
-	// // cambuf[8] = r10; cambuf[9] = -r12; cambuf[10] = r11;
 
 	float cambuf[12] = {};
 	const uint64_t cam_matrix_offset = 0x23FB2B0ull;
